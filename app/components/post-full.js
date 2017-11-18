3809b5e9b50becf086f8410b9bd014ec3d8f6544
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-11-19 01:08:14 +0530

    added post-full and post-tile func

--- app/components/post-full.js
+++ app/components/post-full.js
@@ -1,4 +1,9 @@
-import Component from '@ember/component';
+import Ember from 'ember';
+import stateFor from 'ember-state-services';
+
+const { Component } = Ember;
 
 export default Component.extend({
+  classNames:['post-full'],
+  postInfo: stateFor('post-info', 'model')
 });
--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,4 +1,9 @@
-import Component from '@ember/component';
+import Ember from 'ember';
+import stateFor from 'ember-state-services';
+
+const { Component } = Ember;
 
 export default Component.extend({
+  classNames:['post-tile'],
+  postInfo: stateFor('post-info', 'model')
 });
